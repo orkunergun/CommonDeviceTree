8e0ca9c93526b78efea7a20fd4cda0ac8e5800b8
Orkun Ergün <104510596+orkunergun@users.noreply.github.com>
2022-07-27 16:39:02 +0300

    mt6768-common: Add never allows flag

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -143,6 +143,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # Sepolicy
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+SELINUX_IGNORE_NEVERALLOWS=true
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
