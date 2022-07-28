1c617456fcb05984f0df20067d3ad85306895402
Lunarixus <Nathan@lunarixus.dev>
2022-07-28 14:48:21 +0300

    mt6768-common: Remove packages at build time

--- mt6768-common.mk
+++ mt6768-common.mk
@@ -251,6 +251,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    Remove_Packages
+
 # RIL
 PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
